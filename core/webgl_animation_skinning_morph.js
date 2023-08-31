e1bdc55dc8718a51c82ee5ba669067e83b738839
Leexiaop <282191344@qq.com>
2023-08-31 14:05:45 +0800

    webgl_animation_skinning_morph

--- core/webgl_animation_skinning_morph.js
+++ core/webgl_animation_skinning_morph.js
@@ -1,12 +1,12 @@
-import getSkeletonUtils from '../jsm/utils/SkeletonUtils.js';
+import getSkeletonUtils from '../jsm/utils/SkeletonUtils';
 import * as THREE from '../libs/three.weapp'
 import getGLTFLoader from '../jsm/loaders/GLTFLoader'
 import { OrbitControls } from '../jsm/controls/OrbitControls'
 
-
 module.exports = (canvas) => {
+    let { SkeletonUtils } = getSkeletonUtils(THREE)
     let scene = null, camera = null, renderer = null
-    let controls = null, model = null
+    let controls = null, mixers= [], clock = new THREE.Clock()
     const GLTFLoader = getGLTFLoader(THREE)
     const window = THREE.global
     const init = () => {
@@ -15,8 +15,8 @@ module.exports = (canvas) => {
         scene.fog = new THREE.Fog(0xffffff, 3)
 
         camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000)
-        camera.position.set(2, 3, -10)
-        camera.lookAt(0, 20, 0)
+        camera.position.set(-30, 4, 10)
+        camera.lookAt(0, 10, 10)
         scene.add(camera)
 
         const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, 3)
@@ -48,12 +48,23 @@ module.exports = (canvas) => {
 
         const loader = new GLTFLoader()
         loader.load('https://leexiaop.github.io/static/resources/threejs/models/gltf/RobotExpressive/RobotExpressive.glb', gltf => {
-            model = gltf.scene
-            scene.add(model)
-            model.traverse(object => {
+            gltf.scene.traverse(object => {
                 if (object.isMesh) {
                     object.castShadow = true
                 }
+            })
+            Array.from({length: 14}, (_, index) => {
+                let model = SkeletonUtils.clone(gltf.scene)
+                let mixer = new THREE.AnimationMixer(model)
+                mixer.clipAction(gltf.animations[index]).play()
+                mixers.push(mixer)
+                if (index % 2) {
+                    model.position.x = index * 2
+                }
+                if (!(index % 2)) {
+                    model.position.z = index * 2
+                }
+                scene.add(model)
             })
         })
 
@@ -72,7 +83,9 @@ module.exports = (canvas) => {
     }
     const animate = () => {
         canvas.requestAnimationFrame(animate)
+        const delta = clock.getDelta()
         controls.update()
+        mixers.forEach(mixer => mixer.update(delta))
         renderer.render(scene, camera)
     }
     const onWindowResize = () => {
