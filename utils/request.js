869209b78deaec9355655511d48dc7c588ac8ead
李晓鹏 <lixiaoeng@zstax.com>
2024-06-27 13:59:58 +0800

    dasfshjdajkd

--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,7 @@
 const baseUrl =
 	__wxConfig.envVersion === "develop"
 		? "http://127.0.0.1:5500/"
-        : "https://leexiaop.github.io/";
+		: "https://leexiaop.github.io/";
 
 module.exports = (url) => {
 	if (__wxConfig.envVersion === "develop" && url.indexOf("mini/") !== -1) {
